f174dce33882d5a5f07d15080e16a96e6ba72689
dzarief <dzarief@telcowin.com>
2022-09-15 08:58:18 +0800

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const submodules = [
     require('./modules/discount-code/app/app'),
     require('./modules/discount-redemption-split/app/app'),
+    require('./modules/sms/app/app'),
 ];
 
 const app = express();
